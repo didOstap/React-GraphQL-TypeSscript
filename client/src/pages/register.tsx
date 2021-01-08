b28949ffa6a27c379a726079b15a41ffdf5f36d3
didostap <dribnucko@gmail.com>
2021-01-09 00:39:55 +0200

    client initial commit

---
+++ client/src/pages/register.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+interface RegisterProps {
+
+}
+
+const Register: React.FC<RegisterProps> = ({}) => {
+    return <div>Register Page</div>
+}
+
+export default Register;
